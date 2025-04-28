1620ecd7762814d80c88678774fd5327c5ff6fc9
ali husnain <husnainmcs.dev@gmail.com>
2025-04-28 17:49:48 +0500

    some change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8000,
-    host: "0.0.0.0", // Specify the host to bind to
-    proxy: {
-      "/api": {
-        target: "https://jobs-backend-api-mern-1.onrender.com/api/v1",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
+ plugins: [
+  react({
+   // Use esbuild for JSX transformation
+   babel: {
+    babelrc: false,
+    configFile: false,
+   },
+  }),
+ ],
+ server: {
+  port: 8000,
+  host: '0.0.0.0',
+  proxy: {
+   '/api': {
+    target: 'http://localhost:4000/api/v1',
+    changeOrigin: true,
+    rewrite: (path) => path.replace(/^\/api/, ''),
+   },
   },
+ },
 });
